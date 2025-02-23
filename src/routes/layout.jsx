63aac13192ae363a4c9e4b642e81191bb79471a8
annvhe151009 <annvhe151009@fpt.edu.vn>
2025-02-23 14:18:23 +0700

    update

--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -2,35 +2,66 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../layouts/home/Footer";
 import Navbar from "../layouts/home/Navbar";
 import { Sidebar } from "./dashboard/sidebar";
-import { useEffect, useState } from "react";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { useClickOutside } from "../hooks/use-click-outside";
+import { useEffect, useRef, useState } from "react";
+import { cn } from "../utils/cn";
 import Header from "./dashboard/Header";
 
 const Layout = () => {
   const location = useLocation();
   const isDashboard = location.pathname.startsWith("/dashboard");
-  const [collapsed, setCollapsed] = useState(true);
 
-  // Khi sidebar mở, tắt cuộn trang
+  const isDesktopDevice = useMediaQuery("(min-width: 768px)");
+  const [collapsed, setCollapsed] = useState(!isDesktopDevice);
+  const sidebarRef = useRef(null);
+
   useEffect(() => {
-    if (!collapsed) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
+    setCollapsed(!isDesktopDevice);
+  }, [isDesktopDevice]);
+
+  useClickOutside([sidebarRef], () => {
+    if (!isDesktopDevice && !collapsed) {
+      setCollapsed(true);
+    };
+  });
+
+  useEffect(() => {
+    const scriptUrl = import.meta.env.VITE_TIDIO_SCRIPT_URL;
+    if (!scriptUrl) {
+      console.error("TIDIO script URL is missing from environment variables.");
+      return;
     }
-  }, [collapsed]);
+
+    const script = document.createElement("script");
+    script.src = scriptUrl;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div>
       <Navbar className={isDashboard ? "w-[200px]" : "w-full"} />
       <div className={`w-full ${isDashboard ? "" : "max-w-7xl"} mx-auto`}>
-        <div className="flex w-full px-6">
-          {isDashboard && (
-            <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
-          )}
-          <div className="flex flex-col flex-1">
-            {isDashboard && <Header collapsed={collapsed} setCollapsed={setCollapsed} />}
-            <div className="flex-1">
-              <Outlet />
+        <div className={cn("transision-[margin] duration-300", collapsed ? "md:ml-[30px]" : "md:ml-[30px]")}>
+          <div className="flex w-full px-6">
+            {isDashboard && (
+              <Sidebar
+                ref={sidebarRef}
+                collapsed={collapsed}
+              />
+
+            )}
+            {isDashboard && <div className="w-[1px] bg-gray-700 mx-4" />}
+            <div className="flex flex-col flex-1">
+              {isDashboard && <Header collapsed={collapsed} setCollapsed={setCollapsed} />}
+              <div className="flex-1">
+                <Outlet />
+              </div>
             </div>
           </div>
         </div>
@@ -41,4 +72,3 @@ const Layout = () => {
 };
 
 export default Layout;
-
